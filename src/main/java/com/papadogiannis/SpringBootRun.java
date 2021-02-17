0856420ffa735dc3d2b9844b3c860a9aecdbca3c
lefterispap13 <73543675+lefterispap13@users.noreply.github.com>
2021-02-17 17:07:57 +0200

    new

---
+++ src/main/java/com/papadogiannis/SpringBootRun.java
@@ -0,0 +1,13 @@
+package com.papadogiannis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRun {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringBootRun.class, args);
+    }
+}
---
+++ src/main/java/com/papadogiannis/entities/Account.java
@@ -0,0 +1,9 @@
+package com.papadogiannis.entities;
+
+import javax.persistence.Entity;
+
+
+@Entity
+public class Account {
+
+}
